f2e8e2433961db97f679372ea060dad706830b54
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-03-11 00:59:48 -0500

    v1.33

--- repositories/comments/mo_add.go
+++ repositories/comments/mo_add.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	models "github.com/Aphofisis/po-anfitriones-servicio-informacion-completa/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func Mo_Add(input_comment models.Mo_Comment) error {
@@ -14,7 +15,27 @@ func Mo_Add(input_comment models.Mo_Comment) error {
 	db := models.MongoCN.Database("restoner_anfitriones")
 	col := db.Collection("comment")
 
-	_, err := col.InsertOne(ctx, input_comment)
+	updtString := bson.M{
+		"$set": bson.M{
+			"stars":            input_comment.Stars,
+			"comment":          input_comment.Comment,
+			"idbusiness":       input_comment.IDBusiness,
+			"idcomensal":       input_comment.Comment,
+			"fullnamecomensal": input_comment.FullNameComensal,
+			"fullnamebusiness": input_comment.FullNameBusiness,
+			"phonecomensal":    input_comment.PhoneComensal,
+			"dateregistered":   input_comment.Dateregistered,
+			"isvisible":        input_comment.IsVisible,
+		},
+	}
+
+	filtro := bson.M{
+		"idbusiness": input_comment.IDBusiness,
+		"idcomensal": input_comment.IDComensal,
+	}
+
+	_, err := col.UpdateOne(ctx, filtro, updtString)
+
 	if err != nil {
 		return err
 	}
